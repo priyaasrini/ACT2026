dd727902a17416f93930721e048d231d982a83a7
pvarshinee <priyaa.mirra@gmail.com>
2025-08-04 15:16:56 +0300

    Website files

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  description = "ACT2023 website";
+
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system}; in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            jekyll
+          ];
+        };
+      }
+    );
+}
